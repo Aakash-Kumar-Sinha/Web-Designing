941d33fe14e00d5f017a8e8fb03a702ba2ff8013
Aakash Kumar Sinha <kaakash228@gmail.com>
2026-07-28 13:30:23 +0530

    Add javascript basics

---
+++ JavaScript/01-Basics/script.js
@@ -0,0 +1,22 @@
+// JavaScript Basics
+
+// Printing output
+
+console.log("Hello JavaScript");
+
+
+// Variables introduction
+
+let name = "Developer";
+
+let age = 20;
+
+
+console.log(name);
+
+console.log(age);
+
+
+// Changing content using JavaScript
+
+document.getElementById("message").innerHTML = "JavaScript is working!";
